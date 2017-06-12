e9dfeed1a38007e6deac2f7ab2e33648d358dfbe
Marco Cecconi <marco.cecconi@gmail.com>
2017-06-12 09:45:51 +0100

    Support for invoking functions with parameters. State is preserved across calls.

--- cthulhu.c
+++ cthulhu.c
@@ -6,63 +6,59 @@
 #include <string.h>
 #include "duktape.h"
 
-/*static duk_ret_t js_export(duk_context *ctx) {
-  int n = duk_get_top(ctx);  // #args 
-  printf("argc %d", n);
-  for (int i = 0; i < n; i++) {
-    printf("arg %d: %s\n",i,duk_to_string(ctx, i));
-  }
-  return 1;  // one return value 
-}*/
-
 duk_context *_ctx;
 
-int load_file_to_memory(const char *filename, char ** source_ref) 
+int load_file_to_context(const char *filename) 
 { 
 	size_t size = 0;
 	FILE *f = fopen(filename, "rb");
+  char * source;
+
 	if (f == NULL) 
 	{ 
-    *source_ref = NULL;
+    source = NULL;
+    duk_push_undefined(_ctx);
 		return -1;
 	} 
 	fseek(f, 0, SEEK_END);
 	size = ftell(f);
 	fseek(f, 0, SEEK_SET);
-	*source_ref = (char *)RedisModule_Alloc(size+1);
-	if (size != fread(*source_ref, sizeof(char), size, f)) 
+	source = (char *)RedisModule_Alloc(size);
+	if (size != fread(source, sizeof(char), size, f)) 
 	{ 
-		RedisModule_Free(*source_ref);
-    *source_ref = NULL;
+		RedisModule_Free(source);
+    source = NULL;
+    duk_push_undefined(_ctx);
 		return -2;
 	} 
 	fclose(f);
-	(*source_ref)[size] = 0;
+	duk_push_lstring(_ctx, (const char *) source, (duk_size_t) size);
 	return size;
 }
 
 
 int CthulhuInvoke_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
-  if (argc < 1) {
-    return RedisModule_WrongArity(ctx);
-  }
-
-  duk_dup(_ctx,-1);
 
   size_t length;
 
-  for (int i = 1; i < argc; i++) {
+  if (argc < 2) {
+    return RedisModule_WrongArity(ctx);
+  }
+
+  duk_push_global_object(_ctx);
+  duk_get_prop_string(_ctx, -1 /*index*/, RedisModule_StringPtrLen(argv[1], &length));
+
+  for (int i = 2; i < argc; i++) {
     duk_push_string(_ctx, RedisModule_StringPtrLen(argv[i], &length));
   }
 
-  if (duk_pcall(_ctx, argc - 1) != DUK_EXEC_SUCCESS) {
+  if (duk_pcall(_ctx, argc - 2) != DUK_EXEC_SUCCESS) {
     const char * error = duk_safe_to_string(_ctx, -1);
     duk_pop(_ctx);
     return RedisModule_ReplyWithSimpleString(ctx, error);
   }
   
-  const char * result = duk_get_string(_ctx, -1);
-
+  const char * result = duk_safe_to_string(_ctx, -1);
   duk_pop(_ctx);
 
   if (result == NULL) {
@@ -86,19 +82,24 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) 
   size_t len = 0;
   const char * filename = RedisModule_StringPtrLen(argv[0], &len);
   
-  char * source;
- 
-  if (load_file_to_memory(filename, &source)<0) return REDISMODULE_ERR;
-
+  //TODO: see http://duktape.org/guide.html#error-handling
   _ctx = duk_create_heap_default();
- 
-  if (duk_pcompile_string(_ctx, 0, source) != 0) {
-    printf("compile failed: %s\n", duk_safe_to_string(_ctx, -1));
+  if (!_ctx) {
+    printf("Failed to create a Duktape heap.\n");
     return REDISMODULE_ERR;
   }
 
+  if (load_file_to_context(filename)<0) return REDISMODULE_ERR;
+
+  if (duk_peval(_ctx) != 0) {
+    printf("Compile failed: %s\n", duk_safe_to_string(_ctx, -1));
+    return REDISMODULE_ERR;
+  }
+
+  duk_pop(_ctx);
+
   if (RedisModule_CreateCommand(ctx, "invoke", CthulhuInvoke_RedisCommand, "write", 0, -1, 1) == REDISMODULE_ERR) {
-      return REDISMODULE_ERR;
+    return REDISMODULE_ERR;
   }
 
   return REDISMODULE_OK;
--- test.js
+++ test.js
@@ -1,3 +1,6 @@
-(function(name) {
-    return "Hi " + name + ". In JavaScript land it's " + new Date();
-})(name);
+var count = 1;
+
+function theOldOne ( name ) {
+    return "Hi " + name + ". In JavaScript land it's " + 
+        new Date() + "\nYou invoked the old one " + count++ + " times.";
+}
